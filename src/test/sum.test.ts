a3ea29736ecbfc5a4bed327c4ce8e466c3ec2662
Denesh <denesh@u.nus.edu>
2023-01-25 23:04:36 +0800

    Stop building the test files. Run the test file as ts instead of js

--- src/test/sum.test.ts
+++ src/test/sum.test.ts
@@ -1,13 +1,13 @@
-function sum(a, b) {
+const summation = (a: number, b: number): number => {
   return a + b;
 }
 
 describe("test add function", () => {
   it("should return 15 for add(10,5)", () => {
-    expect(sum(10, 5)).toBe(15);
+    expect(summation(10, 5)).toBe(15);
   });
 
   it("should return 5 for add(2,3)", () => {
-    expect(sum(2, 3)).toBe(5);
+    expect(summation(2, 3)).toBe(5);
   });
 });
