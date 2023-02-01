e1bb913f6485acf22dc77e70d1f403b6a68102fe
Denesh <denesh@u.nus.edu>
2023-02-02 00:28:58 +0800

    Able to read and update the numbers

--- src/controllers/dynamo.ts
+++ src/controllers/dynamo.ts
@@ -1,4 +1,8 @@
 import AWS from 'aws-sdk'
+import { debug } from 'console';
+
+// Set the region 
+AWS.config.update({ region: 'ap-southeast-1' });
 
 export const debug_nums: Array<string> = [];
 export const production_nums: Array<string> = [];
@@ -7,9 +11,6 @@ let debug_nums_modifier = debug_nums
 let production_nums_modifier = production_nums
 
 const obtainNumbers = async () => {
-    // Set the region 
-    AWS.config.update({ region: 'ap-southeast-1' });
-
     // Create the DynamoDB service object
     var ddb = new AWS.DynamoDB()
 
@@ -23,6 +24,11 @@ const obtainNumbers = async () => {
             if (data.Items && data.Items.length === 1) {
                 // In case the data is undefined
                 const debug_nums_string: string = (data.Items[0].numbers.S) ? (data.Items[0].numbers.S) : ("")
+                console.log(`Debug numbers: ${debug_nums_string}`)
+                for (let i = 0; i < debug_nums_modifier.length; i++) {
+                    debug_nums_modifier.pop()
+                }
+
                 debug_nums_string.split(" ").map(element => {
                     debug_nums_modifier.push(element)
                 })
@@ -40,12 +46,80 @@ const obtainNumbers = async () => {
             if (data.Items && data.Items.length === 1) {
                 // In case the data is undefined
                 const production_nums_string: string = (data.Items[0].numbers.S) ? (data.Items[0].numbers.S) : ("")
+                console.log(`Production numbers: ${production_nums_string}`)
+                for (let i = 0; i < production_nums_modifier.length; i++) {
+                    production_nums_modifier.pop()
+                }
                 production_nums_string.split(" ").map(element => {
                     production_nums_modifier.push(element)
                 })
             }
         }
     });
+}
+
+
+export const updateNumbers = ({ debug_nums, old_debug_nums, production_nums, old_production_nums }: { debug_nums: string, old_debug_nums: string, production_nums: string, old_production_nums: string }) => {
+    console.log(`Updating debug: ${debug_nums}`)
+    console.log(`Updating production: ${production_nums}`)
+
+    // Create the DynamoDB service object
+    var ddb = new AWS.DynamoDB()
+
+    ddb.deleteItem({
+        TableName: 'phonenumbers',
+        Key: {
+            'numbers': { S: old_production_nums },
+        }
+    }, (err, data) => {
+        if (err) {
+            console.log("Error:", err);
+        } else {
+            console.log("Success:", data);
+
+            ddb.putItem({
+                TableName: 'phonenumbers',
+                Item: {
+                    'numbers': { S: production_nums },
+                }
+            }, (err, data) => {
+                if (err) {
+                    console.log("Error:", err);
+                } else {
+                    console.log("Success:", data);
+                    obtainNumbers()
+                }
+            });
+        }
+    });
+
+    ddb.deleteItem({
+        TableName: 'debugnumbers',
+        Key: {
+            'numbers': { S: old_debug_nums },
+        }
+    }, (err, data) => {
+        if (err) {
+            console.log("Error:", err);
+        } else {
+            console.log("Success:", data);
+
+            ddb.putItem({
+                TableName: 'debugnumbers',
+                Item: {
+                    'numbers': { S: debug_nums },
+                }
+            }, (err, data) => {
+                if (err) {
+                    console.log("Error:", err);
+                } else {
+                    console.log("Success:", data);
+                    obtainNumbers()
+                }
+            });
+        }
+    });
+
 }
 
 export default obtainNumbers
--- src/routes/adminupdaterouter.ts
+++ src/routes/adminupdaterouter.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from 'express';
 import path from 'path'
+import { updateNumbers } from '../controllers/dynamo';
 
 const adminupdatedrouter = (req: Request, res: Response) => {
+    console.log(JSON.stringify(req.body))
+    updateNumbers({ 
+        old_debug_nums: req.body.old_debug_numbers,
+        debug_nums: req.body.debug_numbers, 
+        old_production_nums: req.body.old_production_nums,
+        production_nums: req.body.production_numbers
+    })
+
     res.render(path.join(__dirname, '../../views/admin_updated'), {
         headline: "Updated Admin Params",
         phone_numbers_debug: req.body.debug_numbers,
