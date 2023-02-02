0162188276d638fd7b492c96bb30fcb5df8d6f71
Denesh <denesh@u.nus.edu>
2023-02-02 12:52:28 +0800

    Adding twilio types

--- src/helper.ts
+++ src/helper.ts
@@ -1,4 +1,5 @@
 import { Twilio } from "twilio"
+import { MessageInstance } from "twilio/lib/rest/api/v2010/account/message"
 
 export const sendMessage = ({ client, body, out_number, sms_id }: {client: Twilio, body: string, out_number: string, sms_id: string}) => {
     client.messages
@@ -7,8 +8,7 @@ export const sendMessage = ({ client, body, out_number, sms_id }: {client: Twili
             to: `${out_number}`,
             messagingServiceSid: `${sms_id}`
         })
-        .then((message: any) => {
-            console.log(message.sid)
-            console.log("sent the message: " + body)
+        .then((message: MessageInstance) => {
+            console.log("sent the message: " + message.body + " from: " + message.sid)
         })
 }
